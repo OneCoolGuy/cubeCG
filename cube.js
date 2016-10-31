1ea4ecd7b9763c4e6a00d5de088316bf7bb1881b
Lucas Germano da Silva <germano@poli.ufrj.br>
2016-10-31 18:19:54 -0200

    trabalho de CG ainda sem mudanca de perspectiva

---
+++ cube.js
@@ -0,0 +1,277 @@
+var canvas;
+var gl;
+
+var numVertices  = 36;
+
+var axisArray = [[1,0,0],
+                  [0,1,0],
+                  [0,0,1]];
+
+
+var rotateFlag = false;
+var axis = 0;
+var xaxis = 0;
+var yaxis =1;
+var zaxis = 2;
+// var thetaRot = [ 0, 0, 0 ];
+var theta = 0.0;
+var thetaLoc;
+var objFlagLoc;
+var objFlag;
+var matrixViewloc,matrixView;
+var rotateMatrixLoc,rotateMatrix;
+var projectionMatrixLoc,projectionMatrix;
+var translateMatrixLoc, translateMatrix;
+var faceLoc;
+
+var eye = vec3(1.0,1.0,1.0);
+const at = vec3(0.0, 0.0, 0.0);
+const up = vec3(0.0, 1.0, 0.0);
+
+var  fovy = 45.0;  // Field-of-view in Y direction angle (in degrees)
+var  aspect;       // Viewport aspect ratio
+
+var near = -10;
+var far = 10;
+var radius = 6.0;
+var theta  = 0.0;
+var phi    = 0.0;
+var dr = 5.0 * Math.PI/180.0;
+
+var left = -2.0;
+var right = 2.0;
+var ytop = 2.0;
+var bottom = -2.0;
+
+var verticesObj = [
+   [  0.0,  0.0,  1.0 ],
+   [  0.0,  1.0,  1.0 ],
+   [  1.0,  1.0,  1.0 ],
+   [  1.0,  0.0,  1.0 ],
+   [  0.0,  0.0,  0.0 ],
+   [  0.0,  1.0,  0.0 ],
+   [  1.0,  1.0,  0.0 ],
+   [  1.0,  0.0,  0.0 ],
+   //axiss object
+   [  0.5,  0.5,  0.5 ],
+   [  1.5,  0.5,  0.5 ],
+   [  0.5,  1.5,  0.5 ],
+   [  0.5,  0.5,  1.5],
+];
+
+var verticesWorld = [
+   //axiss world
+   [  0.0,  0.0,  0.0 ],
+   [  1.5,  0.0,  0.0 ],
+   [  0.0,  1.5,  0.0 ],
+   [  0.0,  0.0,  1.5],
+   //lines for persp
+   [ -10.0,  -0.5,  2.0 ],
+   [  10.0,  -0.5,  2.0 ],
+   [ -10.0,  -0.5, -2.0 ],
+   [  10.0,  -0.5, -2.0 ],
+   ];
+
+
+   // indices of the 12 triangles that compise the cube
+
+var indices = [
+   //cube indices
+   1, 0, 3,
+   3, 2, 1,
+   2, 3, 7,
+   7, 6, 2,
+   3, 0, 4,
+   4, 7, 3,
+   6, 5, 1,
+   1, 2, 6,
+   4, 5, 6,
+   6, 7, 4,
+   5, 4, 0,
+   0, 1, 5,
+   //axis object
+   8, 9,
+   8, 10,
+   8, 11,
+   //axis world
+   12, 13,
+   12, 14,
+   12, 15,
+   //lines for persp
+   16,17,
+   18,19
+   ];
+
+window.onload = function init()
+{
+   canvas = document.getElementById( "gl-canvas" );
+   aspect =  canvas.width/canvas.height;
+   gl = WebGLUtils.setupWebGL( canvas );
+   if ( !gl ) { alert( "WebGL isn't available" ); }
+
+   gl.viewport( 0, 0, canvas.width, canvas.height );
+   gl.clearColor( 0.0, 0.0, 0.0, 1.0 );
+
+   gl.enable(gl.DEPTH_TEST);;
+
+   //
+   //  Load shaders and initialize attribute buffers
+   //
+   var program = initShaders( gl, "vertex-shader", "fragment-shader" );
+   gl.useProgram( program );
+
+   // array element buffer
+
+   var iBuffer = gl.createBuffer();
+   gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, iBuffer);
+   gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint8Array(indices), gl.STATIC_DRAW);
+
+
+   // vertex array attribute buffer
+
+   vertices = verticesObj.concat(verticesWorld);
+   var vBuffer = gl.createBuffer();
+   gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
+   gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW );
+
+   var vPosition = gl.getAttribLocation( program, "vPosition" );
+   gl.vertexAttribPointer( vPosition, 3, gl.FLOAT, false, 0, 0 );
+   gl.enableVertexAttribArray( vPosition );
+
+   thetaLoc = gl.getUniformLocation(program, "theta"); 
+   faceLoc = gl.getUniformLocation(program, "face");
+   objFlagLoc = gl.getUniformLocation(program, "objFlag");
+
+   
+   modelViewMatrixLoc = gl.getUniformLocation( program, "matrixView");
+   gl.uniformMatrix4fv(modelViewMatrixLoc,false, flatten(lookAt(eye,
+               at,
+               up)));
+
+   projectionMatrixLoc = gl.getUniformLocation(program, "projectionMatrix");
+
+   translateMatrixLoc = gl.getUniformLocation( program, "translateMatrix");
+   translateMatrix = translate( -0.5, -0.5, -0.5);
+
+   rotateMatrixLoc = gl.getUniformLocation( program, "rotateMatrix");
+   if(!rotateFlag){
+      rotateMatrix = mat4();
+   }
+
+   
+
+   //event listeners for buttons
+   document.getElementById( "xButton" ).onclick = function () {
+        if(!rotateFlag){
+           rotateFlag = true;
+           axis = 0;;
+        } else {
+           window.cancelAnimationFrame(AnimFrame);
+           axis = 0;
+           updateAxisVert();
+           updateObjVert();
+           theta = 0;
+           init();
+        }
+    };
+    document.getElementById( "yButton" ).onclick = function () {
+        if(!rotateFlag){
+           rotateFlag = true;
+           axis = 1;
+        } else {
+           window.cancelAnimationFrame(AnimFrame);
+           axis = 1;
+           updateAxisVert();
+           updateObjVert();
+           theta = 0;
+           init();
+        }
+    };
+    document.getElementById( "zButton" ).onclick = function () {
+        if(!rotateFlag){
+           rotateFlag = true;
+           axis = 2;
+        } else {
+           window.cancelAnimationFrame(AnimFrame);
+           axis = 2;
+           updateAxisVert();
+           updateObjVert();
+           theta = 0;
+           init();
+        }
+
+    };
+
+   render();
+}
+
+function render()
+{
+   gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+      
+   // gl.uniform3fv(thetaLoc, thetaRot);
+
+   var projectionMatrix = ortho( left, right, bottom, ytop, near, far );
+   // projectionMatrix = perspective(90, 0.1, near, far );
+
+    // gl.uniformMatrix4fv( modelViewMatrixLoc, false, flatten(modelViewMatrix) );
+   gl.uniformMatrix4fv( projectionMatrixLoc, false, flatten(projectionMatrix) );
+   gl.uniformMatrix4fv( translateMatrixLoc, false, flatten(translateMatrix) );
+   gl.uniformMatrix4fv( rotateMatrixLoc, false, flatten(rotateMatrix) );
+
+
+   gl.uniform1i(objFlagLoc, 1);
+   var i = 0;
+   for(i = 0; i<6; i++) {
+      gl.uniform1i(faceLoc, i);
+      gl.drawElements( gl.TRIANGLES, 6, gl.UNSIGNED_BYTE, 6*i );
+   }
+   gl.uniform1i(faceLoc, i++);
+
+   gl.drawElements( gl.LINES, 2, gl.UNSIGNED_BYTE, 36);
+   gl.drawElements( gl.LINES, 2, gl.UNSIGNED_BYTE, 38);
+   gl.drawElements( gl.LINES, 2, gl.UNSIGNED_BYTE, 40);
+
+   gl.uniform1i(objFlagLoc, 0);
+   gl.drawElements( gl.LINES, 2, gl.UNSIGNED_BYTE, 42);
+   gl.drawElements( gl.LINES, 2, gl.UNSIGNED_BYTE, 44);
+   gl.drawElements( gl.LINES, 2, gl.UNSIGNED_BYTE, 46);
+   gl.drawElements( gl.LINES, 2, gl.UNSIGNED_BYTE, 48);
+   gl.drawElements( gl.LINES, 2, gl.UNSIGNED_BYTE, 50);
+
+   if(rotateFlag){
+      rotateMatrix = rotate(theta, axisArray[axis]);
+      theta += 2.0;
+   }  
+
+   AnimFrame = window.requestAnimFrame(render);
+}
+
+function multVec( u , v){
+   result = [];
+   for ( var i = 0; i < u.length; ++i ) {
+      var sum = 0.0;
+      for ( var k = 0; k < u.length; ++k ) {
+         sum += u[i][k] * v[k];
+      }
+      result.push( sum );
+   }
+   return result;
+}
+
+function updateObjVert(){
+   for(var j = 0; j < verticesObj.length; ++j){
+      verticesObj[j] = multVec(translate(-0.5,-0.5,-0.5), verticesObj[j].concat(1));
+      verticesObj[j] = multVec(rotateMatrix, verticesObj[j].concat(1));
+      verticesObj[j] = multVec(translate(0.5,0.5,0.5), verticesObj[j].concat(1));
+      verticesObj[j].pop();
+   }
+}
+
+function updateAxisVert(){
+   for(var j = 0; j < axisArray.length; ++j){
+      axisArray[j] = multVec(rotateMatrix, axisArray[j].concat(1));
+      axisArray[j].pop();
+   }
+}
